fe77a67cd6e4fd2678deb06569ebd194f3e5978c
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-10-17 08:32:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const members = [];
 
-function addMember() {
     let todoListHTML = ``;
+
+function addMember() {
     let inputElement = document.querySelector('.member-input');
     let memberName = inputElement.value;
     let pointsInputElement = document.querySelector('.points-member-input');
-    let numberOfPoints = pointsInputElement;
+    let numberOfPoints = pointsInputElement.value;
     for (let i = 0; i <= members.length; i++) {
         let membersIndex = members[i];
         let html = `
         <div class="lboard_mem">
                             <div class="name_bar">
-                                <p><span>${i + 1}. </span>${members[i]}</p>
+                                <p><span>${i + 1}. </span>${memberName}</p>
                                 <div class="bar_wrap">
                                     <div class="inner_bar" style="width: ${(numberOfPoints / max) * 100}%"></div>
                                 </div>
