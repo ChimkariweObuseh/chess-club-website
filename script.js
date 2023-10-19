aec9d5f700552d4e52110015c2e8bebdf1165b83
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2023-10-19 18:03:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -71,10 +71,17 @@ function addPoints(num) {
 }
 
 function changeClass() {
-    let element = document.querySelector("#plswork");
-    if (element.classList.contains('nodropdown')) {
-    element.classList.replace("nodropdown", "dropdown");
-    } else {
-        element.classList.replace("dropdown", "nodropdown");
+document.getElementById("myDropdown").classList.toggle("show");
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
+  }
+}
 }
